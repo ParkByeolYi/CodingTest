df2ed81f0ba4ce31e1764a076eebea60e1e460f2
ParkByeolYi <51067412+ParkByeolYi@users.noreply.github.com>
2021-05-05 23:58:52 +0900

    Add files via upload

---
+++ CodeUp/6071.py
@@ -0,0 +1,5 @@
+n = 1 #처음 조건 검사를 통과하기 위해 0 아닌 값을 임의로 저장
+while n!=0 : #n이 0이 아닐 경우 True 로 반복문 실행
+  n = int(input())
+  if n!=0 : #n이 0이 아니면 True 로 print() 실행
+    print(n)
---
+++ CodeUp/6072.py
@@ -0,0 +1,4 @@
+n = int(input())
+while n!=0 : #n이 0이 아닐 경우 True 로 반복문 실행
+  print(n)
+  n = n-1 #변수 n의 하나 줄인 값을 변수 n에 다시 저장
